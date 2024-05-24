4e3bac5ffe2a6770ae0353534625090be13395ae
Cody Casterline <cody.casterline@gmail.com>
2024-05-24 10:42:20 -0700

    initial

---
+++ example.ts
@@ -0,0 +1,20 @@
+// Example from: https://github.com/lucacasonato/esbuild_deno_loader?tab=readme-ov-file#example
+
+import * as esbuild from "npm:esbuild@0.20.2";
+// Import the WASM build on platforms where running subprocesses is not
+// permitted, such as Deno Deploy, or when running without `--allow-run`.
+// import * as esbuild from "https://deno.land/x/esbuild@0.20.2/wasm.js";
+
+import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.10.3";
+
+const result = await esbuild.build({
+  plugins: [...denoPlugins()],
+  entryPoints: ["https://deno.land/std@0.185.0/bytes/mod.ts"],
+  outfile: "./dist/bytes.esm.js",
+  bundle: true,
+  format: "esm",
+});
+
+console.log(result.outputFiles);
+
+esbuild.stop();
